ea97557808bc0cd9b331218f9d1e2135cbcaa820
Taylor E. Maryon <taylor.maryon@icloud.com>
2020-10-04 01:20:57 -0600

    updated comments, additional optional design parameters

--- mod2/pokemon/app.js
+++ mod2/pokemon/app.js
@@ -8,6 +8,10 @@ xhr.open("GET", "https://api.vschool.io/pokemon", true)
 // send the request
 xhr.send()
 
+/*  xhr onreadystatechange() method 
+    captures jsonData as responseText(),
+    parses and prepares for showData()
+*/
 xhr.onreadystatechange = () => {
     if(xhr.readyState === 4 && xhr.status === 200) {
         const jsonData = xhr.responseText
@@ -17,6 +21,14 @@ xhr.onreadystatechange = () => {
     }
 }
 
+/* 
+    showData() :: create, fill, and append html elements to DOM
+    parameters: arr (array)
+    textContent filled by .name and .resource_uri from vschool's
+    Pokemon API
+    OPTIONAL CHANGE: accept 2nd argument "element", which defines
+    what element will be created
+*/
 showData = (arr) => {
     for(let i = 0; i < arr.length; i++) {
         // console.log(arr[i].name) // check results
