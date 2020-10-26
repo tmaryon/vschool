d31c1c64f9942e31d172fe83c870067a8947dfce
Taylor E. Maryon <taylor.maryon@icloud.com>
2020-10-26 14:06:40 -0600

    Initial check-in; debugged/completed assign

--- mod2/sand_xhr/app.js
+++ mod2/sand_xhr/app.js
@@ -1,11 +1,11 @@
 
 const xhr = new XMLHttpRequest();
         // method  // url                     // async?
-xhr.open("GET", "https://swapi.co/api/people/1", true)
+xhr.open("GET", "https://swapi.co/api/people/", true)
 xhr.send()
 
-xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4 && xhr.status === 200){
+xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4 && xhr.status === 200) {
         const JSONdata = xhr.responseText
         const data = JSON.parse(JSONdata)
         console.log(data)
